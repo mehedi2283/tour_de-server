81cd96f694cc225b406b2ab85ccb8b1a4e25352f
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-11-10 21:16:14 +0600

    hande 401 and 403

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const cors = require ('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
@@ -16,11 +17,40 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res,next){
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+       return res.send.status(401)({message:'unauthorized access'})
+        
+    }
+
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(error,decoded){
+        if(error){
+            return res.status(403).send({message:'umauthorized access'})
+           
+        }
+        req.decoded = decoded;
+        next()
+    })
+    
+
+
+}
+
 async function run(){
     try{
 
         const serviceCollection = client.db('tourDE').collection('services');
         const reviewCollection = client.db('tourDE').collection('reviews');
+
+
+        app.post('/jwt',(req,res)=>{
+            const user =req.body
+            // console.log(user)
+            const token = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'});
+            res.send({token})
+        })
 
 
 
@@ -49,8 +79,9 @@ async function run(){
 
     });
 
-    //orders api
+    //review api
     app.post('/reviews', async (req,res) =>{
+        
         const review = req.body;
         const result = await reviewCollection.insertOne(review);
         res.send(result);
@@ -68,7 +99,13 @@ async function run(){
         res.send(reviews);
 
 });
-    app.get('/my_reviews', async (req,res) =>{
+    app.get('/my_reviews',verifyJWT, async (req,res) =>{
+        const decoded = req.decoded
+        // console.log('hello',decoded);
+        if(decoded.email !== req.query.email){
+            res.status(403).send({message: 'unauthorized access'})
+        }
+       
         let query = {}
         if(req.query.email){
             query = {
