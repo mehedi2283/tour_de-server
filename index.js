e518bcb25eb9a363510819e8c5ba3d33d193a6d2
Mehedi Hasan <mehedihasan123456789.mh.mh@gmail.com>
2022-11-10 21:57:47 +0600

    remove logs

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run(){
 
         app.post('/jwt',(req,res)=>{
             const user =req.body
-            // console.log(user)
+            
             const token = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'});
             res.send({token})
         })
